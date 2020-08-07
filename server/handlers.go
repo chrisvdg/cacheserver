8024a020a896af2cd8d8d864920c00d5fdabb570
Chris Van de Gejuchte <chris.vandegejuchte@gmail.com>
2020-08-07 17:28:08 +0200

    Adds partial proxy handler

--- server/handlers.go
+++ server/handlers.go
@@ -2,15 +2,22 @@ package server
 
 import (
 	"net/http"
+	"net/url"
+	"path"
 
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
 func newHandlers() *handlers {
-	return &handlers{}
+	return &handlers{
+		http: &http.Client{},
+	}
 }
 
 type handlers struct {
+	proxyBaseURL string
+	http         *http.Client
 }
 
 func (h *handlers) CacheHandler(res http.ResponseWriter, req *http.Request) {
@@ -18,5 +25,45 @@ func (h *handlers) CacheHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 func (h *handlers) ProxyHandler(res http.ResponseWriter, req *http.Request) {
-	log.Errorf("Proxy Handler")
+	log.Debug("Proxy Handler")
+
+	targetURL, err := h.getProxyURL(req.URL.Path)
+	if err != nil {
+		h.handleError(res, req, err)
+		return
+	}
+	targetReq, err := http.NewRequest(req.Method, targetURL, req.Body)
+	if err != nil {
+		h.handleError(res, req, err)
+		return
+	}
+
+	for name, values := range req.Header {
+		for _, v := range values {
+			targetReq.Header.Add(name, v)
+		}
+	}
+
+	targetResp, err := h.http.Do(targetReq)
+	if err != nil {
+		h.handleError(res, req, errors.Wrap(err, "target request failed"))
+	}
+	_ = targetResp
+
+	// TODO: write target response to response to the client
+
+}
+
+func (h *handlers) handleError(res http.ResponseWriter, req *http.Request, err error) {
+	log.Error(err)
+	res.WriteHeader(http.StatusInternalServerError)
+}
+
+func (h *handlers) getProxyURL(reqPath string) (string, error) {
+	u, err := url.Parse(h.proxyBaseURL)
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to fetch path from request")
+	}
+	u.Path = path.Join(u.Path, reqPath)
+	return u.String(), nil
 }
